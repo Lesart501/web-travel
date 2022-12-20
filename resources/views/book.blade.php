eeeb3302edd2defeaddbc53ed6ae3a9987b2f5cf
Lesart501 <artem11sent2002@mail.ru>
2022-12-20 18:02:43 +0300

    Translated forms

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -3,26 +3,28 @@
 @section('title', 'Бронирование тура')
 
 @section('content')
-<div class="container">
-        <a href="{{ route('index') }}" class="btn btn-primary m-5">Вернуться</a>
-    </div>
-    <div class="container d-flex justify-content-center text-center">
-        <div class="card" style="width: 20rem;">
-        <form action="" method="post"></form>
-            <div class="card-body">
-                <h5 class="card-title">{{$tour->name}}, {{$tour->country->name}}</h5>
-                <p class="card-text">{{$tour->nights}} ночей, {{$tour->people}} человека</p>
-                <p class="card-text">{{$tour->price}} р.</p>
-                <p class="card-text">{{$tour->operator->name}}</p>
-                <form action="{{ route('book', ['tour' => $tour->id]) }}" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label for="txtTitle">Дата отправления</label>
-                        <input type="date" name="out_date" id="txtTitle" class="form-control mt-2 mb-4">
-                    </div>
+    <div class="container-xxl d-flex justify-content-evenly my-5">
+        <img class="card-img-top img-fluid w-50" src="../../storage/uploads/{{$tour->image}}">
+        <div class="ml-3">
+            <h5 class="card-title">{{$tour->name}}, {{$tour->country->name}}</h5>
+            <p class="card-text my-4"><b>Ночей:</b>    {{$tour->nights}}</p>
+            <p class="card-text"><b>Человек:</b>    {{$tour->people}}</p>
+            <form action="{{ route('book', ['tour' => $tour->id]) }}" method="post">
+                @csrf
+                <div class="form-group my-4">
+                    <label for="txtTitle">Дата отправления</label>
+                    <input type="date" name="out_date" id="txtTitle" class="form-control mt-2 mb-4">
+                </div>
+                <div class="form-group my-4">
+                    <label for="txtTitle">Дата отправления</label>
+                    <input type="date" name="return_date" id="txtTitle" class="form-control mt-2">
+                </div>
+                <h4 class="card-text text-primary">{{$tour->price}} р.</h4>
+                <div class="my-4">
                     <input type="submit" value="Забронировать" class="btn btn-primary">
-                </form>
-            </div>
+                    <a href="{{ route('index') }}" class="btn btn-primary">Вернуться</a>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
