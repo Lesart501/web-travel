564e18636eb7078e7a3e1aa05be48712a4e1af51
Lesart501 <artem11sent2002@mail.ru>
2024-07-13 01:42:17 +0300

    ide-helper installed

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $statuses_id
+ * @property int $users_id
+ * @property int $tours_id
+ * @property string $out_date
+ * @property string $return_date
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Status $status
+ * @property-read \App\Models\Tour $tour
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereOutDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereReturnDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereStatusesId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereToursId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUsersId($value)
+ * @mixin \Eloquent
+ */
 class Order extends Model
 {
     public function status() {
@@ -13,7 +38,7 @@ class Order extends Model
     public function user() {
         return $this->belongsTo(User::class, 'users_id', 'id');
     }
-    
+
     public function tour() {
         return $this->belongsTo(Tour::class, 'tours_id', 'id');
     }
